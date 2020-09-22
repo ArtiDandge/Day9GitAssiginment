42b495604c1bdcf2a5579daa012840b302f01d25
ArtiDandge <58045752+ArtiDandge@users.noreply.github.com>
2020-09-22 23:23:41 +0530

    calculate datily wages

--- empWageComputation.sh
+++ empWageComputation.sh
@@ -2,11 +2,18 @@
 
 echo "Welcome to Employee Wage Computation Program"
 
+#Constants
+WAGES_PER_HOUR=20
+FULL_DAY_WORKHOURS=8
+
 attendence=$((RANDOM%2))
+
 
 if [[ $attendence -eq 1 ]]
 then
 	echo "Employee Present"
+	dailyWages=$(($WAGES_PER_HOUR * $FULL_DAY_WORKHOURS))
 else
 	echo "Employee Absent"
 fi
+
