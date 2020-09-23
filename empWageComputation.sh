58a6c124673f530228b01548edb9576dd73cf3a2
ArtiDandge <58045752+ArtiDandge@users.noreply.github.com>
2020-09-23 11:56:39 +0530

    Calculate monthly wages

--- empWageComputation.sh
+++ empWageComputation.sh
@@ -6,14 +6,19 @@ echo "Welcome to Employee Wage Computation Program"
 WAGES_PER_HOUR=20
 FULL_DAY_WORKHOURS=8
 PART_TIME_HOURS=4
+DAYS_PER_MONTH=20
 attendence=$((RANDOM%3))
 
 
 case $attendence in
-	1)Wages=$(($WAGES_PER_HOUR * $FULL_DAY_WORKHOURS));
+	1)dailyWages=$(($WAGES_PER_HOUR * $FULL_DAY_WORKHOURS))
+	  MonthlyWage=$(($dailyWages * $DAYS_PER_MONTH));;
 	
-	2)Wages=$(($WAGES_PER_HOUR * $PART_TIME_HOURS));
+	2)dailyWages=$(($WAGES_PER_HOUR * $PART_TIME_HOURS))
+	 MonthlyWage=$(($dailyWages * $DAYS_PER_MONTH));;
 
 	*)Wages=0
+	 MonthlyWage=0
+
 esac
-	echo $attendence 
+	echo $MonthlyWage
