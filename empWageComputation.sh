ac02b8b71af7007ac0edffc517fb63f20bb9d4a7
ArtiDandge <58045752+ArtiDandge@users.noreply.github.com>
2020-09-23 12:18:15 +0530

    calculate wages untill condition fail

--- empWageComputation.sh
+++ empWageComputation.sh
@@ -7,18 +7,35 @@ WAGES_PER_HOUR=20
 FULL_DAY_WORKHOURS=8
 PART_TIME_HOURS=4
 DAYS_PER_MONTH=20
+
+MAX_WORKING_DAYS=20;
+MAX_WORKING_HOURS=100;
 attendence=$((RANDOM%3))
 
+#Variables
+declare -i workDonePerDay
+totalWorkingDays=0;
+totalWorkingHours=0;
 
 case $attendence in
-	1)dailyWages=$(($WAGES_PER_HOUR * $FULL_DAY_WORKHOURS))
-	  MonthlyWage=$(($dailyWages * $DAYS_PER_MONTH));;
+	1)dailyWages=$(($WAGES_PER_HOUR * $FULL_DAY_WORKHOURS));;
 	
-	2)dailyWages=$(($WAGES_PER_HOUR * $PART_TIME_HOURS))
-	 MonthlyWage=$(($dailyWages * $DAYS_PER_MONTH));;
-
-	*)Wages=0
-	 MonthlyWage=0
-
+	2)dailyWages=$(($WAGES_PER_HOUR * $PART_TIME_HOURS));;
+	
+	*)dailyWages=0
+	
 esac
-	echo $MonthlyWage
+	echo $dailyWages
+
+MonthlyWage=$(($dailyWages * $DAYS_PER_MONTH));
+
+
+while [[ $totalWorkingHours -lt $MAX_WORKING_HOURS && $totalWorkingDays -lt $MAX_WORKING_DAYS ]]
+do
+	((totalWorkingDays++));
+	((totalWorkingHours++));
+	Wages=$(($WAGES_PER_HOUR * ($totalWorkingDays * $totalWorkingHours)));
+	#getting store per day work hours in Dictionary
+	TotalWages[$totalWorkingDays]=$Wages;
+done
+	echo ${TotalWages[@]}
